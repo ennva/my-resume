60e7b2edfbff3ec5b89439c7ab73cad7f850bfd4
Ennvaweb <35902574+ennva@users.noreply.github.com>
2020-11-28 20:53:16 +0100

    Update curriculum.component.ts
    Update about section

--- src/app/curriculum/curriculum.component.ts
+++ src/app/curriculum/curriculum.component.ts
@@ -22,11 +22,11 @@ export class CurriculumComponent implements OnInit {
     phone: '(+39)051/6171646'
   }
   email = 'nephthali2017@gmail.com';
-  about = 'Engineering Technician with an outstanding quality record of engineering products from design to fabrication.' +
+  about = 'IT Consultant with an outstanding quality record of engineering products from design to fabrication.' +
     'Team-oriented and self-directed Engineer with personal integrity and professional ethics.' +
-    'Adept at managing projects and resolving issues in software and hardware troubleshooting.' +
+    'Experienced in software engineering specially on software design, development and lifecycle management.' +
     'CompTia Cloud+ certified leveraging cloud business and technical solutions.' +
-    'SAP Business suite Specialist and good understanding of many programming languages';
+    'Good understanding of Database Management and many programming languages';
 
   resume = null;
   message = "";
